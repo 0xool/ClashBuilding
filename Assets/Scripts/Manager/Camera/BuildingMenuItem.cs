fe95cdfdb20fe12701030c27837be49c052f1bf6
cyrusrefahi@gmail.com <cyrusrefahi@gmail.com>
2023-02-10 19:05:17 -0800

    +Adds minor bug fixes regarding drag and drop

--- Assets/Scripts/Manager/Camera/BuildingMenuItem.cs
+++ Assets/Scripts/Manager/Camera/BuildingMenuItem.cs
@@ -19,16 +19,16 @@ public class BuildingMenuItem :
 
     void Update() {
         if(isBuilding){
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        int mask =  LayerMask.GetMask("MainPlane");
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            int mask =  LayerMask.GetMask("MainPlane");
 
-        if (Physics.Raycast(ray, out hit, 1000.0f, mask))
-        {
-            if(hit.transform.gameObject.tag == "MainPlane"){    
-                this.build.transform.position = new Vector3(hit.point.x, hit.point.y + this.build.transform.localScale.y / 2, hit.point.z);
+            if (Physics.Raycast(ray, out hit, 1000.0f, mask))
+            {
+                if(hit.transform.gameObject.tag == "MainPlane"){    
+                    this.build.transform.position = new Vector3(hit.point.x, hit.point.y + this.build.transform.localScale.y / 2, hit.point.z);
+                }
             }
-        }
         }    
     }
 
